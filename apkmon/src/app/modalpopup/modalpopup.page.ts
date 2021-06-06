d417814acce2e8fbc8835081bc7e0d2ed830922f
Jean Santos <--global>
2021-06-06 19:53:50 -0300

    Melhorias graficas na pagina 1 e popUp, adição do hide/unhide

--- apkmon/src/app/modalpopup/modalpopup.page.ts
+++ apkmon/src/app/modalpopup/modalpopup.page.ts
@@ -7,8 +7,9 @@ import {ModalController} from '@ionic/angular';
   styleUrls: ['./modalpopup.page.scss'],
 })
 export class ModalpopupPage implements OnInit {
-  public topic1:boolean = false;
-  public topic2:boolean = false;
+  public hideMe:boolean = false;
+  public hideMe1:boolean = false;
+  public hideMe2:boolean = false;
 
   constructor(private modalController:ModalController) { }
 
@@ -17,5 +18,35 @@ export class ModalpopupPage implements OnInit {
   CloseModal()
   {
     this.modalController.dismiss();
+  }
+  hide() {
+    switch(this.hideMe){
+      case true:
+        this.hideMe = false;
+        break;
+      case false:
+        this.hideMe = true;
+        break;
+    }
+  }
+  hide1() {
+    switch(this.hideMe1){
+      case true:
+        this.hideMe1 = false;
+        break;
+      case false:
+        this.hideMe1 = true;
+        break;
+    }
+  }
+  hide2() {
+    switch(this.hideMe2){
+      case true:
+        this.hideMe2 = false;
+        break;
+      case false:
+        this.hideMe2 = true;
+        break;
+    }
   }
 }
--- apkmon/src/app/tab1/tab1.page.ts
+++ apkmon/src/app/tab1/tab1.page.ts
@@ -8,10 +8,63 @@ import {ModalpopupPage} from '../modalpopup/modalpopup.page';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  public hideMe:boolean = false;
+  public hideMe1:boolean = false;
+  public hideMe2:boolean = false;
+  public hideMe3:boolean = false;
+  public hideMe4:boolean = false;
   constructor(private modalController:ModalController) {}
   OpenModal(){
     this.modalController.create({component:ModalpopupPage}).then((modalElement)=>{modalElement.present();})
   }
-
+  hide() {
+    switch(this.hideMe){
+      case true:
+        this.hideMe = false;
+        break;
+      case false:
+        this.hideMe = true;
+        break;
+    }
+  }
+  hide1() {
+    switch(this.hideMe1){
+      case true:
+        this.hideMe1 = false;
+        break;
+      case false:
+        this.hideMe1 = true;
+        break;
+    }
+  }
+  hide2() {
+    switch(this.hideMe2){
+      case true:
+        this.hideMe2 = false;
+        break;
+      case false:
+        this.hideMe2 = true;
+        break;
+    }
+  }
+  hide3() {
+    switch(this.hideMe3){
+      case true:
+        this.hideMe3 = false;
+        break;
+      case false:
+        this.hideMe3 = true;
+        break;
+    }
+  }
+  hide4() {
+    switch(this.hideMe4){
+      case true:
+        this.hideMe4 = false;
+        break;
+      case false:
+        this.hideMe4 = true;
+        break;
+    }
+  }
 }
