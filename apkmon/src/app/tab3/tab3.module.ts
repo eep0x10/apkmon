d08b9079b24f1155469cbb58cf8611873a4d0ee2
Hugomathe <matheushugo59@gmail.com>
2021-06-21 18:14:27 -0300

    modalevo

--- apkmon/src/app/tab3/tab3.module.ts
+++ apkmon/src/app/tab3/tab3.module.ts
@@ -7,7 +7,7 @@ import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
-
+import { ModalevoPageModule } from './../modalevo/modalevo.module';
 @NgModule({
   imports: [
     IonicModule,
@@ -16,6 +16,7 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     ExploreContainerComponentModule,
     RouterModule.forChild([{ path: '', component: Tab3Page }]),
     Tab3PageRoutingModule,
+    ModalevoPageModule,
   ],
   declarations: [Tab3Page]
 })
--- apkmon/src/app/tab3/tab3.page.ts
+++ apkmon/src/app/tab3/tab3.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { PokemonService } from '../services/pokemon.service';
+import {ModalController} from '@ionic/angular';
+import {ModalevoPage} from '../modalevo/modalevo.page';
 
 @Component({
   selector: 'app-tab3',
@@ -9,7 +11,7 @@ import { PokemonService } from '../services/pokemon.service';
 })
 export class Tab3Page implements OnInit {
 
-  constructor(private pokeService: PokemonService) {}
+  constructor(private pokeService: PokemonService,private modalController:ModalController) {}
   ngOnInit() {
     this.refresh();
   }
@@ -21,4 +23,65 @@ export class Tab3Page implements OnInit {
   public refresh(){
     setInterval(()=> this.setPokemon(), 5000);
   }
+
+  public hideMe:boolean = false;
+  public hideMe1:boolean = false;
+  public hideMe2:boolean = false;
+  public hideMe3:boolean = false;
+  public hideMe4:boolean = false;
+
+  OpenModal(){
+    this.modalController.create({component:ModalevoPage}).then((modalElement)=>{modalElement.present();})
+  }
+  hide() {
+    switch(this.hideMe){
+      case true:
+        this.hideMe = false;
+        break;
+      case false:
+        this.hideMe = true;
+        break;
+    }
+  }
+  hide1() {
+    switch(this.hideMe1){
+      case true:
+        this.hideMe1 = false;
+        break;
+      case false:
+        this.hideMe1 = true;
+        break;
+    }
+  }
+  hide2() {
+    switch(this.hideMe2){
+      case true:
+        this.hideMe2 = false;
+        break;
+      case false:
+        this.hideMe2 = true;
+        break;
+    }
+  }
+  hide3() {
+    switch(this.hideMe3){
+      case true:
+        this.hideMe3 = false;
+        break;
+      case false:
+        this.hideMe3 = true;
+        break;
+    }
+  }
+  hide4() {
+    switch(this.hideMe4){
+      case true:
+        this.hideMe4 = false;
+        break;
+      case false:
+        this.hideMe4 = true;
+        break;
+    }
+  }
 }
+
