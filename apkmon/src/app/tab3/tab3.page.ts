907e7b6ed49bdfe67c0908e911e66b644cfe6e91
GustavoNascimentoMatos <79610465+GustavoNascimentoMatos@users.noreply.github.com>
2021-06-21 22:57:32 -0300

    Revert "Merge branch 'dev' of https://github.com/eep0x10/apkmon into dev"
    This reverts commit a7923491327904205b90e7d926397fec68b0c795, reversing
    changes made to 13272e4a8e8633161d98487dc7a4a86afd1c2ad4.

--- apkmon/src/app/tab3/tab3.page.ts
+++ apkmon/src/app/tab3/tab3.page.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { PokemonService } from '../services/pokemon.service';
-import {ModalController} from '@ionic/angular';
-import {ModalevoPage} from '../modalevo/modalevo.page';
 
 @Component({
   selector: 'app-tab3',
@@ -11,7 +9,7 @@ import {ModalevoPage} from '../modalevo/modalevo.page';
 })
 export class Tab3Page implements OnInit {
 
-  constructor(private pokeService: PokemonService,private modalController:ModalController) {}
+  constructor(private pokeService: PokemonService) {}
   ngOnInit() {
     this.refresh();
   }
@@ -23,65 +21,4 @@ export class Tab3Page implements OnInit {
   public refresh(){
     setInterval(()=> this.setPokemon(), 5000);
   }
-
-  public hideMe:boolean = false;
-  public hideMe1:boolean = false;
-  public hideMe2:boolean = false;
-  public hideMe3:boolean = false;
-  public hideMe4:boolean = false;
-
-  OpenModal(){
-    this.modalController.create({component:ModalevoPage}).then((modalElement)=>{modalElement.present();})
-  }
-  hide() {
-    switch(this.hideMe){
-      case true:
-        this.hideMe = false;
-        break;
-      case false:
-        this.hideMe = true;
-        break;
-    }
-  }
-  hide1() {
-    switch(this.hideMe1){
-      case true:
-        this.hideMe1 = false;
-        break;
-      case false:
-        this.hideMe1 = true;
-        break;
-    }
-  }
-  hide2() {
-    switch(this.hideMe2){
-      case true:
-        this.hideMe2 = false;
-        break;
-      case false:
-        this.hideMe2 = true;
-        break;
-    }
-  }
-  hide3() {
-    switch(this.hideMe3){
-      case true:
-        this.hideMe3 = false;
-        break;
-      case false:
-        this.hideMe3 = true;
-        break;
-    }
-  }
-  hide4() {
-    switch(this.hideMe4){
-      case true:
-        this.hideMe4 = false;
-        break;
-      case false:
-        this.hideMe4 = true;
-        break;
-    }
-  }
 }
-
